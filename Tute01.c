d4d62d1b0921b0ec79c66a695c137cea012cc700
IT21158704 Dilshan K.T.D.N <100205314+IT21158704@users.noreply.github.com>
2022-03-06 18:50:13 +0000

    Completed

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,17 @@
 #include <stdio.h>
 
 int main() {
-  
+    
+	float subject1, subject2;
+	
+	printf("Enter Subject 1 Result : ");
+	scanf("%f",&subject1);
+	
+	printf("Enter Subject 2 Result : ");
+	scanf("%f",&subject2);
+	
+	printf("\nAverage = %.2f",(subject1 + subject2 )/2);
+	
   return 0;
 }
 
